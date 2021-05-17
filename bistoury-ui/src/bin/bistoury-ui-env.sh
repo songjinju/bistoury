a1b38204fcab4ba9dfc14abc6a640747e780f8c9
songjinju <447140705@qq.com>
2021-05-17 14:55:16 +0800

    修改pid获取方式

--- bistoury-ui/src/bin/bistoury-ui-env.sh
+++ bistoury-ui/src/bin/bistoury-ui-env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-JAVA_HOME="/tmp/bistoury/java"
+JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.282.b08-1.el7_9.x86_64"
 JAVA_OPTS="-Dbistoury.conf=$BISTOURY_COF_DIR -Dbistoury.cache=$BISTOURY_CACHE_DIR"
